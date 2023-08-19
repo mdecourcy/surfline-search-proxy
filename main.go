e56cda61b3b0bdd339093b6873bcc0d3ec38b4d5
Mac DeCourcy <github.znq26@slmail.me>
2023-08-19 14:59:16 -0700

    more robust error handling, logging, etc

--- main.go
+++ main.go
@@ -1,21 +1,68 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 
 const (
-	surflineAPI = "https://services.surfline.com/search/site"
-	port        = ":8080"
+	defaultPort    = "8080"
+	defaultTimeout = 10 * time.Second
+	surflineAPI    = "https://services.surfline.com/search/site"
+)
+
+var (
+	httpClient = &http.Client{Timeout: defaultTimeout}
 )
 
 func main() {
-	http.HandleFunc("/search", handleSearch)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
 
-	log.Println("Server started on", port)
-	log.Fatal(http.ListenAndServe(port, nil))
+	server := &http.Server{
+		Addr:    ":" + port,
+		Handler: http.HandlerFunc(loggingMiddleware(handleSearch)),
+	}
+
+	go func() {
+		log.Println("Server started on", port)
+		if err := server.ListenAndServe(); err != http.ErrServerClosed {
+			log.Fatalf("Server ListenAndServe error: %v", err)
+		}
+	}()
+
+	// Graceful shutdowns with a timeout.
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
+
+	<-stop
+
+	ctx, cancel := context.WithTimeout(context.Background(), defaultTimeout)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Printf("Error during server shutdown: %v", err)
+	}
+
+	log.Println("Server stopped gracefully")
+}
+
+func loggingMiddleware(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
+		next.ServeHTTP(w, r)
+
+		log.Printf("%s %s %s Completed in %v", r.RemoteAddr, r.Method, r.URL.Path, time.Since(start))
+	}
 }
 
 func handleSearch(w http.ResponseWriter, r *http.Request) {
@@ -25,15 +72,24 @@ func handleSearch(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	resp, err := http.Get(surflineAPI + "?q=" + query + "&querySize=10&suggestionSize=10&newsSearch=true")
+	resp, err := httpClient.Get(surflineAPI + "?q=" + query + "&querySize=10&suggestionSize=10&newsSearch=true")
 	if err != nil {
+		log.Printf("Failed to fetch data from Surfline API: %v", err)
 		http.Error(w, "Error fetching data from Surfline API", http.StatusInternalServerError)
 		return
 	}
 	defer resp.Body.Close()
 
+	if resp.StatusCode != http.StatusOK {
+		errMsg := fmt.Sprintf("Surfline API returned unexpected status: %v", resp.Status)
+		log.Println(errMsg)
+		http.Error(w, errMsg, http.StatusInternalServerError)
+		return
+	}
+
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
+		log.Printf("Error reading response from Surfline API: %v", err)
 		http.Error(w, "Error reading response from Surfline API", http.StatusInternalServerError)
 		return
 	}
